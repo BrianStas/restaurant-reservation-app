17900ef39379639169d36ae560515b66ca671394
Brian Stasiukaitis <brianstas@gmail.com>
2023-12-04 14:48:22 -0500

    updated Validator
    Cleared up validator and added console log to test the date functionality as it appears in render

--- back-end/src/errors/validatorFor.js
+++ back-end/src/errors/validatorFor.js
@@ -2,17 +2,11 @@ const validator = require('validator');
 
 
 function validatorFor(property) {
-    // const removeTime = (date = new Date()) => {
-    //     return new Date(
-    //       date.getFullYear(),
-    //       date.getMonth(),
-    //       date.getDate()
-    //     );
-    //   }
-    // today.getFullYear().toString()
+
     return function (req, res, next){
     if(property == "reservation_date"){
         const reservationDate = new Date(`${req.body.data[property]}T${req.body.data.reservation_time}`);
+        console.log("reservation date is: ", reservationDate, "new Date looks like: ", new Date())
         if (reservationDate < new Date()) {
             return res.status(400).send({ error: 'reservation_date should be in the future' })
           }
@@ -21,21 +15,6 @@ function validatorFor(property) {
         if(givenDay===2){
             return res.status(400).send({ error: 'Business is closed on Tuesdays' })
             }
-        
-        // const today = new Date();
-        // const todayString = (today.getFullYear().toString() + today.getMonth().toString() + today.getDate().toString())
-        // const givenDate = new Date(req.body.data[property]); 
-        // const givenDateString = (givenDate.getFullYear().toString()+ givenDate.getMonth().toString()) + (givenDate.getDate().toString())
-        
-        // console.log("given date: ", givenDateString, "Today: ", todayString);
-        // if((Number(givenDateString)+1) < Number(todayString)){
-        //     return res.status(400).send({ error: 'reservation_date must be a future date'});
-        // }
-        // const dayOfGivenDate = givenDate.getDay();
-        // if(dayOfGivenDate === 1){
-        //     console.log("Tuesday found: ", givenDate);
-        //     return res.status(400).send({ error: 'closed on Tuesdays'});
-        // }
 
         const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
         dateRegex.test(req.body.data[property]) ? next() : res.status(400).send({ error: 'reservation_date should be in correct format' })
